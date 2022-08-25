839356db06131bc6d81654cf861425b7903b6f0f
SabareeshAH <sabareeshhari2003@gmail.com>
2022-08-25 22:31:45 +0530

    Error Message Added to Model 2

--- server/model/VehicleHistory.js
+++ server/model/VehicleHistory.js
@@ -4,44 +4,44 @@ const Schema = mongoose.Schema;
 const VehicleHistory = new Schema({
     vehicle_id: {
         type: mongoose.SchemaTypes.ObjectId,
-        required: true
+        required: [true,"Vechile ID is Required"]
     },
     user_id: {
         type: mongoose.SchemaTypes.ObjectId,
-        required: true
+        required: [true,"User ID is Required"]
     },
     pickup_location: {
         type: String,
-        required: true
+        required: [true,"Pick UP Location is Mandatory"]
     },
     pickup_date_and_time: {
         type: Date,
-        required: true
+        required: [true,"Pick UP Date and Time are Nescessary"]
     },
     dropoff_date_and_time: {
         type: Date,
-        required: true
+        required: [true,"Drop Off Date and Time are Nescessary"]
     },
     mode_of_payment: {
         type: String,
-        required: true
+        required: [true,"Payment mode must be mentioned"]
     },
     payment_details: {
         type: String,
-        required: true,
-        unique: true
+        required: [true,"Payment Details are Required for Booking"],
+        unique: [true,"Payment Details should be Unique"]
     },
     price: {
         type: Number,
-        required: true
+        required: [true,"Price paid is Required"]
     },
     distance_travelled: {
         type: Number,
-        required: true
+        required: [true,"Distance Travelled by Vechile is Mandatory"]
     },
     booking_status: {
         type: String,
-        required: true
+        required: [true,"Booking Status is Important to notify the Users"]
     },
     Rating: Number,
 
